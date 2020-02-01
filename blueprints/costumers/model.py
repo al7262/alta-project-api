de6b2de090fb32d3c6232cd16a24beb7d90e41a8
Gading09 <hedy@alterra.id>
2020-02-01 17:02:28 +0700

    add database costumers

---
+++ blueprints/costumers/model.py
@@ -0,0 +1,35 @@
+from blueprints import db
+from flask_restful import fields
+import datetime
+
+class Costumers(db.Model):
+    __tablename__ = "Costumer"
+    id = db.Column(db.Integer, primary_key = True, autoincrement = True)
+    fullname = db.Column(db.String(150), nullable = False, default = "")
+    phone_number = db.Column(db.String(20), nullable = False, default = "")
+    email = db.Column(db.String(150), nullable = False, default = "")
+    image = db.Column(db.String(150), nullable = False, default = "")
+    created_at = db.Column(db.DateTime, default = datetime.datetime.now())
+
+    
+    response_fields = {
+        'id' : fields.Integer,
+        'fullname' : fields.String,
+        'phone_number' : fields.String,
+        'email' : fields.String,
+        'image' : fields.String,
+        'created_at' : fields.DateTime
+    }
+
+    jwt_claims_fields = {
+        'id' : fields.Integer
+    }
+
+    def __init__(self, fullname, phone_number, email, image):
+        self.fullname = fullname
+        self.phone_number = phone_number
+        self.email = email
+        self.image = image
+
+    def __repr__(self):
+        return '<Costumer %r>' %self.id
