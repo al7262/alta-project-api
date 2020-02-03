ea3b81c7e58cae9ebbceb9076f0107c5e8a29e00
Garry Ariel <garry@alterra.id>
2020-02-03 15:19:40 +0700

    Fix Auth Issue

--- blueprints/inventories/resources.py
+++ blueprints/inventories/resources.py
@@ -10,6 +10,7 @@ import json
 
 # Import Authentication
 from flask_jwt_extended import jwt_required, get_jwt_claims
+from blueprints import dashboard_required, user_required, apps_required
 
 # Creating blueprint
 bp_inventories = Blueprint('inventories', __name__)
@@ -21,6 +22,82 @@ class InventoryResource(Resource):
         return {'status': 'ok'}, 200
 
     # Get all inventories for specified owner
+    @jwt_required
+    def get(self):
+        # Take input from users
+        claims = get_jwt_claims()
+        id_user = claims['id']
+        parser = reqparse.RequestParser()
+        parser.add_argument('name', location = 'json', required = True)
+        args = parser.parse_args()
+
+        # Get all inventories
+        inventories = Inventories.query.filter_by(id_users = id_user).filter_by(deleted = 0)
+
+        # Filter by inventory name
+        if args['name'] != '':
+            inventories = inventories.filter(Inventories.name.like("%" + args['name'] + "%"))
+        
+        # Show the result
+        result = []
+        for inventory in inventories:
+            inventory = marshal(inventory, Inventories.inventories_fields)
+            inventory['stock'] = inventory['total_stock']
+            inventory['status'] = "Available"
+
+            # Get the stock outlet
+            id_inventory = inventory['id']
+            stock_outlet_list = StockOutlet.query.filter_by(id_inventory = id_inventory)
+            for stock_outlet in stock_outlet_list:
+                stock_outlet = marshal(stock_outlet, StockOutlet.response_fields)
+                if stock_outlet['stock'] <= stock_outlet['reminder']:
+                    inventory['status'] = "Warning"
+
+            result.append(inventory)
+        return result, 200
+
+class InventoryPerOutlet(Resource):
+    # Enable CORS
+    def options(self, id=None):
+        return {'status': 'ok'}, 200
+
+    # Get all inventories in an outlet
+    @jwt_required
+    @dashboard_required
+    def get(self, id_outlet):
+        pass
+        # # Take input from users
+        # parser = reqparse.RequestParser()
+        # parser.add_argument('name', location = 'json', required = True)
+        # args = parser.parse_args()
+
+        # # Get all inventories in an outlet
+        # inventories = Inventories.query.filter_by(id_outlet = id_outlet).filter_by(deleted = False)
+
+        # # Get all related stock outlet
+        # result = []
+        # for inventory in inventories:
+        #     inventory = marshal(inventory, Inventories.inventories_fields)
+        #     id_inventory = inventory['id_inventory']
+        #     stock_outlet = StockOutlet.query.filter_by(id_inventory = id_inventory).filter_by(id_outlet = id_outlet).first()
+
+        #     # Check remainder status
+        #     if int(stock_outlet['stock']) <= int(stock_outlet['reminder']):
+        #         remainder = 'Warning'
+        #     else:
+        #         remainder = 'Available'
+
+        #     # Prepare the data to be shown
+        #     data = {
+        #         'name': inventory['name'],
+        #         'unit': inventory['unit'],
+        #         'unit_price': inventory['unit_price'],
+        #         'stock': stock_outlet.stock,
+        #         'status': remainder
+        #     }
+        #     result.append(data)
+        
+        # return result, 200
 
 class InventoryLogResource(Resource):
     # Enable CORS
@@ -28,4 +105,5 @@ class InventoryLogResource(Resource):
         return {'status': 'ok'}, 200
 
 api.add_resource(InventoryResource, '')
+api.add_resource(InventoryPerOutlet, '/<id_outlet>')
 api.add_resource(InventoryLogResource, '/log')
