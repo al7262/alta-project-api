166ad60d8942dc989f400a645d02b661453ffca1
Gading09 <hedy@alterra.id>
2020-02-05 22:01:17 +0700

    modif dashboard

--- blueprints/customers/model.py
+++ blueprints/customers/model.py
@@ -12,7 +12,7 @@ class Customers(db.Model):
     fullname = db.Column(db.String(150), nullable = False, default = "")
     phone_number = db.Column(db.String(20), nullable = False, default = "")
     email = db.Column(db.String(150), nullable = False, default = "")
-    total_transaction = db.Column(db.Integer, nullable = False, default = "")
+    total_transaction = db.Column(db.Integer, nullable = False, default = 0)
     created_at = db.Column(db.DateTime, default = datetime.datetime.now())
 
     response_fields = {
