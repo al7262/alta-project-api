21742406abb0f5d0de6df6d67e433e6052c3d440
Garry Ariel <garry@alterra.id>
2020-02-07 10:09:03 +0700

    Test

--- simple_ci.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-# Push from local
-sudo docker rmi garryarielcussoy/easykachin:be
-alta123
-sudo docker build -t garryarielcussoy/easykachin:be .
-sudo docker push garryarielcussoy/easykachin:be
