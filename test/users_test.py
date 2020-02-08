7ef48a49b43f1112bbf40bd06c1511aac6f5ef61
Gading09 <hedy@alterra.id>
2020-02-08 21:09:31 +0700

    modif

--- test/users_test.py
+++ test/users_test.py
@@ -1,72 +1,72 @@
-# import json
-# from . import app, client, create_token, db_reset, cache
+import json
+from . import app, client, create_token, db_reset, cache
 
-# class TestUsers():
-#     # Show users profile
-#     def test_users_profile(self, client):
-#         # Prepare the DB and token
-#         db_reset()
-#         token = create_token('hedy@alterra.id')
+class TestUsers():
+    # Show users profile
+    def test_users_profile(self, client):
+        # Prepare the DB and token
+        db_reset()
+        token = create_token('hedy@alterra.id')
 
-#         # Test the endpoints
-#         res = client.get('/user/profile', headers={'Authorization': 'Bearer ' + token})
-#         res_json = json.loads(res.data)
-#         assert res.status_code == 200
+        # Test the endpoints
+        res = client.get('/user/profile', headers={'Authorization': 'Bearer ' + token})
+        res_json = json.loads(res.data)
+        assert res.status_code == 200
     
-#     def test_valid_edit_profile(self, client):
-#         # Prepare the DB and token
-#         db_reset()
-#         token = create_token('hedy@alterra.id')
+    def test_valid_edit_profile(self, client):
+        # Prepare the DB and token
+        db_reset()
+        token = create_token('hedy@alterra.id')
 
-#         # Prepare the data to be inputted
-#         data = {
-#             "fullname": "Hedy Gading",
-#             "password": "Gading09",
-#             "phone_number": "089511447629",
-#             "business_name": "Mie-Niac",
-#             "image": "https://dummy.jpg"
-#         }
+        # Prepare the data to be inputted
+        data = {
+            "fullname": "Hedy Gading",
+            "password": "Gading09",
+            "phone_number": "089511447629",
+            "business_name": "Mie-Niac",
+            "image": "https://dummy.jpg"
+        }
 
-#         # Test the endpoints
-#         res = client.put('/user/profile', json = data, headers={'Authorization': 'Bearer ' + token})
-#         res_json = json.loads(res.data)
-#         assert res.status_code == 200
+        # Test the endpoints
+        res = client.put('/user/profile', json = data, headers={'Authorization': 'Bearer ' + token})
+        res_json = json.loads(res.data)
+        assert res.status_code == 200
     
-#     def test_edit_profile_not_match_password(self, client):
-#         # Prepare the DB and token
-#         db_reset()
-#         token = create_token('hedy@alterra.id')
+    def test_edit_profile_not_match_password(self, client):
+        # Prepare the DB and token
+        db_reset()
+        token = create_token('hedy@alterra.id')
 
-#         # Prepare the data to be inputted
-#         data = {
-#             "fullname": "Hedy Gading",
-#             "password": "justgading",
-#             "phone_number": "089511447629",
-#             "business_name": "Mie-Niac",
-#             "image": "https://dummy.jpg"
-#         }
+        # Prepare the data to be inputted
+        data = {
+            "fullname": "Hedy Gading",
+            "password": "justgading",
+            "phone_number": "089511447629",
+            "business_name": "Mie-Niac",
+            "image": "https://dummy.jpg"
+        }
 
-#         # Test the endpoints
-#         res = client.put('/user/profile', json = data, headers={'Authorization': 'Bearer ' + token})
-#         res_json = json.loads(res.data)
-#         assert res.status_code == 422
+        # Test the endpoints
+        res = client.put('/user/profile', json = data, headers={'Authorization': 'Bearer ' + token})
+        res_json = json.loads(res.data)
+        assert res.status_code == 422
         
-#     def test_edit_profile_empty_field(self, client):
-#         # Prepare the DB and token
-#         db_reset()
-#         token = create_token('hedy@alterra.id')
+    def test_edit_profile_empty_field(self, client):
+        # Prepare the DB and token
+        db_reset()
+        token = create_token('hedy@alterra.id')
 
-#         # Prepare the data to be inputted
-#         data = {
-#             "fullname": "",
-#             "password": "Gading09",
-#             "phone_number": "089511447629",
-#             "business_name": "Mie-Niac",
-#             "image": "https://dummy.jpg"
-#         }
+        # Prepare the data to be inputted
+        data = {
+            "fullname": "",
+            "password": "Gading09",
+            "phone_number": "089511447629",
+            "business_name": "Mie-Niac",
+            "image": "https://dummy.jpg"
+        }
 
-#         # Test the endpoints
-#         res = client.put('/user/profile', json = data, headers={'Authorization': 'Bearer ' + token})
-#         res_json = json.loads(res.data)
-#         assert res.status_code == 400
-#         assert res_json['message'] == 'Tidak boleh ada kolom yang dikosongkan'
+        # Test the endpoints
+        res = client.put('/user/profile', json = data, headers={'Authorization': 'Bearer ' + token})
+        res_json = json.loads(res.data)
+        assert res.status_code == 400
+        assert res_json['message'] == 'Tidak boleh ada kolom yang dikosongkan'
